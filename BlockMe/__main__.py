c1040d4304e329458927f0b213072ab007d2b5e7
LinearGap <102157470+LinearGap@users.noreply.github.com>
2022-03-30 21:26:50 +0100

    hosts interface working

--- BlockMe/__main__.py
+++ BlockMe/__main__.py
@@ -1,6 +1,12 @@
 from hosts import hosts as Host
 
 if __name__ == '__main__':
-    hosts = Host()
-    print(hosts.read_backup())
-    hosts.write_backup()
+    def main():
+        hosts = Host()
+        print(hosts.get())
+        hosts.add('www.facebook.co.uk')
+        hosts.add_list(['google.com', 'next.com', 'youtube.com', 'more.com'])
+        print(hosts.get())
+        hosts.save()
+        return 0
+    main()
--- BlockMe/hosts.py
+++ BlockMe/hosts.py
@@ -11,9 +11,28 @@ class hosts:
             'Darwin': '/etc/hosts',
             'Windows': 'c:\windows\system32\drivers\etc\hosts'}
 
+    # The header to write in host file above any added hosts
+    header = '# BlockMe hosts. Run BlockMe stop to remove'
+
+    # The footer after the hosts have been added
+    footer = '# End of BlockME hosts.'
+
+    # The IP address to use as a redirect
+    ip_redirect = '127.0.0.1'
+
     def __init__(self):
-        self.__filepath = hosts.path[self.__get_os()]
-        self.__backup_filepath = self.__filepath + '_BACKUP'
+        # Paths of the files on this system        
+        self.__hosts_filepath = hosts.path[self.__get_os()]
+        self.__hosts_backup_filepath = self.__hosts_filepath + '.bmb'
+        self.__first_run_master_backup_filepath = self.__hosts_filepath + '_FRMB.bmb'
+
+        # List of sites to block when writing the hosts file
+        self.__blocked_hosts = []
+
+        # Read the current hosts file and save into backup file and
+        # also into system_hosts for use later
+        self.__system_hosts = self.__read_current_hosts()
+        self.__write_backup_hosts(self.__system_hosts)
 
     def __get_os(self):
         """
@@ -36,29 +55,125 @@ class hosts:
 
     def __write_file(self, filepath, contents):
         """
-        Write the supplied contents into a file. Replaces anyhting currently 
+        Write the supplied contents into a file. Replaces anything currently 
         there. Does not append.
         """
         hosts_file = open(filepath, 'w+')
         with hosts_file:
             hosts_file_contents = hosts_file.writelines(contents)
 
-    def read_current(self):
+    def __read_current_hosts(self):
         try:
-            return self.__read_file(self.__filepath)
+            return self.__read_file(self.__hosts_filepath)
         except:
             return None
 
-    def read_backup(self):
+    def __read_backup_hosts(self):
         try:
-            return self.__read_file(self.__backup_filepath)
+            return self.__read_file(self.__hosts_backup_filepath)
         except:
             return None
 
-    def write_backup(self):
+    def __write_backup_hosts(self, contents):
         """
-        Replace this
+        Write the supplied contents into the backup hosts file
         """
-        h = self.read_current()
-        self.__write_file(self.__backup_filepath, h)
+        try:
+            self.__write_file(self.__hosts_backup_filepath, contents)
+        except:
+            raise
+
+    def __write_first_run_master_backup_hosts(self, contents):
+        """
+        Write the supplied contents into the first run master backup
+        hosts file. Only to be used on first run of the programm
+        """
+        try:
+            self.__write_file(self.__write_first_run_master_backup_hosts, contents)
+        except:
+            raise
+
+    def __format_hosts(self):
+        """
+        Creates an output list for saving to hosts appending header
+        and footer into the output along with the current hosts file
+        """
+        output = []
+        try:
+            output.extend(self.__system_hosts)
+            output.append('\n##\n')
+            output.append(hosts.header + '\n')
+            output.append('#\n')
+            # Loop through the blocked address list and create string to append to the list
+            for addr in self.__blocked_hosts:
+                print(addr)
+                line = hosts.ip_redirect + '    ' + addr + '\n'
+                output.append(line)
+                print(line)
+            output.append('#\n')
+            output.append(hosts.footer + '\n')
+            output.append('##\n')
+        except:
+            raise
+
+        return output
+
+    ### Public API
+
+    def get(self):
+        """
+        Returns the list of hosts store in the blocked hosts lists
+        does not return anything from the system. Only what is managed
+        by this script
+        """
+        return self.__blocked_hosts
+
+    def add(self, *addr):
+        """
+        Adds the address: addr to the blocked hosts list 
+        """
+        for address in addr:
+            self.__blocked_hosts.append(address)
+
+    def add_list(self, addr_list):
+        """
+        Adds the supplied list of hosts to the blocked hosts list
+        """
+        self.__blocked_hosts.extend(addr_list)
+
+    def remove(self, addr):
+        """
+        Remove the addr from the blocked hosts lists,
+        addr could be in blocked hosts more than once so,
+        loop until this is not the fact
+        """
+        while(addr in self.__blocked_hosts):
+            self.__blocked_hosts.remove(addr)
+
+    def remove_list(self, addr_list):
+        """
+        Remove the addr in list from the blocked hosts lists,
+        addr could be in blocked hosts more than once so,
+        loop until this is not the fact
+        """
+        for addr in addr_list:
+            self.remove(addr)
+        
+    def save(self):
+        """
+        Saves the contents of the blocked hosts lists along with the header
+        and footer into the hosts file on the system
+        """
+        try:
+            self.__write_file(self.__hosts_filepath, self.__format_hosts())
+        except:
+            raise
+
+    def restore(self):
+        """
+        Restores the hosts file to the system state from before anything 
+        was written to it
+        """
+        backup = self.__read_backup_hosts()
+        self.__write_file(self.__hosts_filepath, backup)
         
