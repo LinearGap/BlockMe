a67e0dcb2ee4ea9b75c67dc64b46ce189aca4570
LinearGap <102157470+LinearGap@users.noreply.github.com>
2022-03-31 15:13:35 +0100

    default config

--- BlockMe/config.py
+++ BlockMe/config.py
@@ -6,4 +6,29 @@ class config():
     """
 
     def __init__(self):
+        # Config objects
+        self.redirect_addr = '' # The ip address to use for redirection
+        self.last_state = '' # The last state the application was in. Used to detect bad shutdown and reset before continuing
+        self.scheduler_pid = 0 # The PID of the scheduler running process. To manage subprocess
+        
+        self.permanent_urls = [] # List of urls to be permanently blocked when the blocker runs
+        self.scheduled_urls = [] # List of {'url':, 'start_time':, 'end_time':} dicts for scheduled blocking
+
+    # Public API
+    def load(self, config_path):
+        """
+        Load the config from the file at config_path.
+        """
+        pass
+
+    def save(self, config_path):
+        """
+        Save the config to the file at config_path
+        """
+        pass
+
+    def restore_default(self, config_path):
+        """
+        Restore the default config file to config_path
+        """
         pass
